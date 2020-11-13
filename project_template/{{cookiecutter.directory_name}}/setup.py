56f23e13697764c79512bc7289fce7f42cf31215
Biogitte <birgittenilsson@live.dk>
2020-11-13 20:39:45 +0100

    Fixed cookiecutter variable names.

--- project_template/{{cookiecutter.directory_name}}/setup.py
+++ project_template/{{cookiecutter.directory_name}}/setup.py
@@ -5,5 +5,5 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_name }}',
+    author='{{ cookiecutter.author }}',
     license='{% if cookiecutter.license == "MIT" %}MIT{% else %}Equancy{% endif %}')
